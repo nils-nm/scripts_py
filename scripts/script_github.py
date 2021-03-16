da9b9cf985c1d309c8b3339b7feb26eb77b883ed
nils <nilsfuruskog@yandex.ru>
2021-03-16 22:57:10 +0300

    add minor functional

--- scripts/script_github.py
+++ scripts/script_github.py
@@ -7,13 +7,20 @@ while True:
     f = ''
     print('началось выполнение задачи мы в ', os.getcwd())
     print('проверка в каком мы ветвлении и статуса файлов')
-    # os.system('git status')
-    # os.system('git branch')
-    # a = input('если вас все устраивает нажмите enter иначе введите имя ветвления в которое вы хотите попасть')
-    for i in sub.getoutput('git branch'):
-        f += i
-    if '* main' in f:
-        print(f)
+    os.system('git status')
+    os.system('git branch')
+    a = input('если вас все устраивает нажмите enter иначе введите имя ветвления в которое вы хотите попасть \n~ ')
+    if not a == 'master':
+        for i in sub.getoutput('git branch'):
+            f += i
+        if a in f:
+            os.system('git checkout ' + a)
+        elif a == '* ' + a:
+            print('already ')
+        else:
+            os.system('git checkout -b ' + a)
+        os.system('git checkout ' + a)
+
     output_branch = sub.getoutput('ls ~/PycharmProjects/mahlab/task')
 
     # print(output_branch)
